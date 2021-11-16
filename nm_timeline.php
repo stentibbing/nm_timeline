f98aa0c3780881282e2777ed33a4ee1b65c24351
stentibbing <sten.tibbing@gmail.com>
2021-11-16 15:58:35 +0200

    Changed styles to hide mobile view on desktop

--- nm_timeline.php
+++ nm_timeline.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Nordic Milk Timeline
  * Plugin URI:        https://www.taifuun.ee
  * Description:       This plugin adds timeline custom post type for event creation. For displaying in public, use nm_timeline shortcode.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Author:            Sten Tibbing
  * Author URI:        https://www.kbuum.com
  * License:           GPL-2.0+
@@ -26,7 +26,7 @@ if (! defined('WPINC')) {
 /**
  * Currently plugin version.
  */
-define('NM_TIMELINE_VERSION', '1.0.0');
+define('NM_TIMELINE_VERSION', '1.0.1');
 
 /**
  * The code that runs during plugin activation.
